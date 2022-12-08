4c717d01152afd64d4be9e2d30bb869e33d01bf3
Koala610 <vladli2000610@gmail.com>
2022-12-08 18:47:46 +0600

    add: limit controller base, limit request template edit: limit service and limit

--- src/main/java/com/test/microservice/service/LimitService.java
+++ src/main/java/com/test/microservice/service/LimitService.java
@@ -8,6 +8,7 @@ import com.test.microservice.service.exchangeRates.ExchangeRatesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +24,7 @@ public class LimitService {
 
 
     public Limit createLimit(Limit limit) {
+        limit.setLimitDatetime(new Date(System.currentTimeMillis()));
         return limitRepository.save(limit);
     }
 
