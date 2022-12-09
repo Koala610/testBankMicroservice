15e5d0d08d043e299154bd616c3738c6881e21ea
Koala610 <vladli2000610@gmail.com>
2022-12-10 00:40:56 +0600

    add: docker configuration

--- src/main/java/com/test/microservice/config/CassandraConfiguration.java
+++ src/main/java/com/test/microservice/config/CassandraConfiguration.java
@@ -1,6 +1,7 @@
 package com.test.microservice.config;
 
 import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.internal.core.metadata.DefaultEndPoint;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +17,9 @@ public class CassandraConfiguration {
     private String keyspace;
     @Value("${cassandra.port}")
     private int port;
+    @Value("${cassandra.address}")
+    private String address;
     public @Bean CqlSession session() {
-        return CqlSession.builder().withKeyspace(keyspace).addContactPoint(new InetSocketAddress(port)).withLocalDatacenter(datacenter).build();
+        return CqlSession.builder().withKeyspace(keyspace).withLocalDatacenter(datacenter).addContactEndPoint(new DefaultEndPoint(new InetSocketAddress(address, port))).build();
     }
 }
